f9c3c6ab0c5d05fda7b5db8fbc352c5eb09dc140
bikash-789 <chauhanbikash789@gmail.com>
2021-12-05 19:57:10 +0545

    Print_Subsequence

---
+++ Recursion_2/Print_Subsequence.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+
+void subs(string input, string output)
+{
+    //base case
+    if(input.size()==0)
+    {
+        cout<<output<<endl;
+        return;
+    }
+    //recursive call
+
+    subs(input.substr(1), output);
+    subs(input.substr(1), output+input[0]);
+
+}
+
+int main()
+{
+    string input;
+    cin>>input;
+    subs(input, "");
+
+}
