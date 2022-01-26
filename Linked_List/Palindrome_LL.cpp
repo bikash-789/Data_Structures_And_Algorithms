e36363f09d04c1d15e0353efce14554edd98f0fe
bikash-789 <chauhanbikash789@gmail.com>
2022-01-27 00:00:33 +0545

    Check Palindrome LL

---
+++ Linked_List/Palindrome_LL.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+
+bool isPalindrome(Node* head)
+{
+    //base case
+    if(!head) return true;
+
+    //recursive call
+    bool isPalinSmall = isPalindrome(head->next);
+    Node *lastNode = head;
+    while(lastNode->next != NULL)
+    {   
+        lastNode=lastNode->next;
+    }
+    if(head->data == lastNode->data) return true;
+    return false;
+}
+
+int main()
+{
+	int t;
+	cin >> t;
+
+	while (t--)
+	{
+		Node *head = takeinput();
+		bool ans = isPalindrome(head);
+
+		if (ans) cout << "true";
+		else cout << "false";
+
+		cout << endl;
+	}
+
+	return 0;
+}
