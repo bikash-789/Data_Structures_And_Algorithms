c8ae3c72baac17b9189b905a9fc9b19a75228af2
bikash-789 <chauhanbikash789@gmail.com>
2022-01-26 23:43:12 +0545

    EliminateDuplicates in LL

---
+++ Linked_List/Eliminate_Duplicates.cpp
@@ -0,0 +1,87 @@
+/*
+You have been given a singly linked list of integers where the elements are sorted in ascending order. Write a function that removes the consecutive duplicate values such that the given list only contains unique elements and returns the head to the updated list.
+ Input format :
+The first line contains an Integer 't' which denotes the number of test cases or queries to be run. Then the test cases follow.
+
+The first and the only line of each test case or query contains the elements(in ascending order) of the singly linked list separated by a single space.
+
+*/
+
+#include <iostream>
+
+class Node
+{
+public:
+	int data;
+	Node *next;
+	Node(int data)
+	{
+		this->data = data;
+		this->next = NULL;
+	}
+};
+
+using namespace std;
+
+Node *takeinput()
+{
+	int data;
+	cin >> data;
+	Node *head = NULL, *tail = NULL;
+	while (data != -1)
+	{
+		Node *newnode = new Node(data);
+		if (head == NULL)
+		{
+			head = newnode;
+			tail = newnode;
+		}
+		else
+		{
+			tail->next = newnode;
+			tail = newnode;
+		}
+		cin >> data;
+	}
+	return head;
+}
+
+void print(Node *head)
+{
+	Node *temp = head;
+	while (temp != NULL)
+	{
+		cout << temp->data << " ";
+		temp = temp->next;
+	}
+	cout << endl;
+}
+
+Node* removeDuplicates(Node* head)
+{
+    //base case
+    if(head==NULL) return head;
+
+    //recursive call
+    Node *smallAns = removeDuplicates(head->next);
+
+    //small calculation
+    if(head->next && head->data == head->next->data)
+    {
+        return smallAns;
+    }
+    else head->next = smallAns;
+    return head;
+}
+int main()
+{
+	int t;
+	cin >> t;
+	while (t--)
+	{
+		Node *head = takeinput();
+		head = removeDuplicates(head);
+		print(head);
+	}
+	return 0;
+}
